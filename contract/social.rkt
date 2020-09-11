8bd2de523d949fd9ec29eb55eaf7f0da57bcd4e3
Siddhartha <sid@countvajhula.com>
2020-09-10 18:15:22 -0700

    a few more contracts

--- contract/social.rkt
+++ contract/social.rkt
@@ -6,10 +6,13 @@
 
 (provide
  (contract-out [function/c (binary-composition/c contract?)]
+               [thunk/c (self-map/c contract?)]
                [binary-function/c (-> contract? contract? contract? contract?)]
                [variadic-function/c (-> contract? contract? contract?)]
+               [predicate/c (->* () (contract?) contract?)]
                [encoder/c (self-map/c contract?)]
                [decoder/c (self-map/c contract?)]
+               [hash-function/c (thunk/c contract?)]
                [maybe/c (->* (contract?) (contract?) contract?)]
                [binary-composition/c (self-map/c contract?)]
                [variadic-composition/c (self-map/c contract?)]
@@ -17,6 +20,7 @@
                [self-map/c (self-map/c contract?)]
                [functional/c (->* () (contract?) contract?)]
                [binary-constructor/c (binary-composition/c contract?)]
+               [classifier/c (->* () (contract?) contract?)]
                [variadic-constructor/c (binary-composition/c contract?)]
                [variadic-comparison-predicate/c (self-map/c contract?)]
                [variadic-comparison-selection/c (self-map/c contract?)]))
@@ -24,17 +28,26 @@
 (define (function/c source/c target/c)
   (-> source/c target/c))
 
+(define (thunk/c target/c)
+  (-> target/c))
+
 (define (binary-function/c a/c b/c target/c)
   (-> a/c b/c target/c))
 
 (define (variadic-function/c source/c target/c)
   (-> source/c ... target/c))
 
+(define (predicate/c [on-type/c any/c])
+  (function/c on-type/c boolean?))
+
 (define (encoder/c as-type/c)
   (function/c any/c as-type/c))
 
 (define (decoder/c from-type/c)
   (function/c from-type/c any/c))
+
+(define (hash-function/c)
+  (encoder/c fixnum?))
 
 (define (maybe/c contract [default/c #f])
   (or/c contract default/c))
@@ -56,6 +69,11 @@
 
 (define (binary-constructor/c primitive/c composite/c)
   (binary-function/c primitive/c composite/c composite/c))
+
+(define (classifier/c [by-type/c any/c])
+  (-> (encoder/c by-type/c)
+      list?
+      (listof list?)))
 
 (define (variadic-constructor/c primitive/c composite/c)
   (variadic-function/c primitive/c composite/c))
