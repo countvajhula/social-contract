070ed1d397fd4a24f0a84606962cdae393b33696
Siddhartha <sid@countvajhula.com>
2020-10-09 11:44:57 -0700

    test..

--- tests/social.rkt
+++ tests/social.rkt
@@ -10,21 +10,22 @@
          (only-in racket/function thunk)
          "private/util.rkt")
 
-(define tests
-  (check-true #t)
-  ;; (test-suite
-  ;;  "Tests for social contracts"
+(module+ test
+  (define tests
+    (check-true #t)
+    ;; (test-suite
+    ;;  "Tests for social contracts"
 
-  ;;  (test-case
-  ;;      "Unary function contract"
-  ;;    (define/contract (list-length lst)
-  ;;      (function/c list? natural-number/c)
-  ;;      (if (eq? lst null)
-  ;;          0
-  ;;          (add1 (list-length (rest lst)))))
-  ;;    (check-equal? (list-length '(h e l l o)) 5)
-  ;;    (check-exn exn:fail:contract? (thunk (list-length "hello")))))
-  )
+    ;;  (test-case
+    ;;      "Unary function contract"
+    ;;    (define/contract (list-length lst)
+    ;;      (function/c list? natural-number/c)
+    ;;      (if (eq? lst null)
+    ;;          0
+    ;;          (add1 (list-length (rest lst)))))
+    ;;    (check-equal? (list-length '(h e l l o)) 5)
+    ;;    (check-exn exn:fail:contract? (thunk (list-length "hello")))))
+    ))
 
 (module+ test
   (just-do
