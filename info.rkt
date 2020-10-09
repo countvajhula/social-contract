661e726e63981b4d9b9b97ba7aeaab56cf1c5418
Siddhartha <sid@countvajhula.com>
2020-10-09 16:48:16 -0700

    test..

--- info.rkt
+++ info.rkt
@@ -9,12 +9,6 @@
                      "cover"
                      "cover-coveralls"
                      "sandbox-lib"))
-(define compile-omit-paths '("dev" "tests" "coverage"))
-(define test-include-paths '("tests"))
-(define clean '("contract/compiled"
-                "contract/social/compiled"
-                "contract/social/doc"
-                "contract/social/doc/social-contract"))
 (define pkg-desc "Common contracts.")
 (define version "0.0")
 (define pkg-authors '(countvajhula))
