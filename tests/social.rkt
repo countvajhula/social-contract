ea171569e6597254308e2b0fccce6c6e5728c311
Siddhartha <sid@countvajhula.com>
2020-10-09 16:53:35 -0700

    test..

--- tests/social.rkt
+++ tests/social.rkt
@@ -3,32 +3,27 @@
 (module+ test
   (require rackunit
            rackunit/text-ui
-           ;; (except-in racket/contract
-           ;;            predicate/c)
-           ;; (only-in racket/list
-           ;;          rest)
-           ;; contract/social
-           ;; (only-in racket/function thunk)
+           (except-in racket/contract
+                      predicate/c)
+           (only-in racket/list
+                    rest)
+           contract/social
+           (only-in racket/function thunk)
            "private/util.rkt"))
 
 (module+ test
-  (define tests
-    (test-suite
-     "Tests for social contracts"
-     (check-true #t))
-    ;; (test-suite
-    ;;  "Tests for social contracts"
+  (test-suite
+   "Tests for social contracts"
 
-    ;;  (test-case
-    ;;      "Unary function contract"
-    ;;    (define/contract (list-length lst)
-    ;;      (function/c list? natural-number/c)
-    ;;      (if (eq? lst null)
-    ;;          0
-    ;;          (add1 (list-length (rest lst)))))
-    ;;    (check-equal? (list-length '(h e l l o)) 5)
-    ;;    (check-exn exn:fail:contract? (thunk (list-length "hello")))))
-    ))
+   (test-case
+       "Unary function contract"
+     (define/contract (list-length lst)
+       (function/c list? natural-number/c)
+       (if (eq? lst null)
+           0
+           (add1 (list-length (rest lst)))))
+     (check-equal? (list-length '(h e l l o)) 5)
+     (check-exn exn:fail:contract? (thunk (list-length "hello"))))))
 
 (module+ test
   (just-do
